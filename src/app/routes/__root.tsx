bd7bacd9b9d66e540dc9f50ed33035bb51e4f05a
EmmanuelBeja <beja.emmanuel@gmail.com>
2025-09-17 03:05:46 +0300

    feat: implement page layout

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,4 +1,6 @@
 // RootRoute: layout, header, outlet
+import Footer from '@/components/shared/layout/Footer';
+import { Navbar } from '@/components/shared/layout/Navbar';
 import ScrollToTop from '@/components/shared/ScrollToTop';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 
@@ -7,9 +9,11 @@ export const Route = createRootRoute({
     return (
       <div className="w-full">
         <ScrollToTop />
+        <Navbar />
         <div className="mx-auto p-4 max-w-6xl">
           <Outlet />
         </div>
+        <Footer />
       </div>
     );
   },
---
+++ src/components/shared/layout/Navbar.tsx
@@ -0,0 +1,35 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import { Link } from '@tanstack/react-router';
+
+export function Navbar() {
+  return (
+    <NavigationMenu className="justify-center bg-primary">
+      <div className="flex justify-between p-4 w-full max-w-6xl">
+        <Link to="/" className="font-extrabold text-[30px] text-white">
+          🍿 movies
+        </Link>
+        <NavigationMenuList className="flex items-center space-x-2">
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link
+                to="/login"
+                className="bg-transparent hover:bg-primary/60 hover:drop-shadow-lg font-semibold text-[18px] text-white hover:text-secondary"
+              >
+                Login
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </div>
+    </NavigationMenu>
+  );
+}
